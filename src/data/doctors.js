89dece5f4ca957d42a45930b3c453a2fe00b5d26
Vasundhara Ravikumar <vasundhararavi641@gmail.com>
2025-05-13 16:55:23 +0200

    Testing update
    Added a few doctors information manually to doctors.js for testing purposes

--- src/data/doctors.js
+++ src/data/doctors.js
@@ -3,14 +3,28 @@ export const doctors = [
     id: 1,
     name: "Dr. Alice Johnson",
     specialty: "Pediatrics",
-    availability: ["Monday", "Thursday"],
-    image: "/images/doctor1.jpg"
+    availability: ["Monday", "Thursday"]
+
   },
   {
     id: 2,
     name: "Dr. Bob Smith",
     specialty: "Cardiology",
-    availability: ["Tuesday", "Friday"],
-    image: "/images/doctor2.jpg"
+    availability: ["Tuesday", "Friday"]
+    
+  },
+  {
+    id: 3,
+    name: "Dr. Shaun Murphy",
+    specialty: "Neurology",
+    availability: ["Wednesday", "Saturday"]
+  },
+  {
+    id: 4,
+    name: "Dr. Audrey Lim",
+    specialty : "Trauma Surgeon",
+    availability: ["Monday", "Tuesday", "Wednesday", "Thursday"]
   }
+
+
 ];
