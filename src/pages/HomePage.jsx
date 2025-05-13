3f8aab3563f8b18b202e1488b9222004bab89031
Vasundhara Ravikumar <vasundhararavi641@gmail.com>
2025-05-13 16:56:34 +0200

    Load doctors function
    Used useState and useEffect functions to display the doctors on the homepage. Implemented the doctors to be filtered based on their specialization

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,68 @@
+import { doctors } from "../data/doctors";
+import { useState, useEffect } from 'react';
+
+
 function Homepage(){
-    return <h1>Coming soon!</h1>
+    //Sets the state to use all the doctors available
+    const [allDoctors, setAllDoctors] = useState(doctors);
+
+    //Sets state to filter out the doctors
+    const [filteredDoctors, setFilteredDoctors] = useState(doctors);
+
+    //Store doctors based on their rspecialization
+    const [selectedSpecialty, setSelectedSpecialty] = useState(''); //Blank for the user to select from the dropdown
+
+    useEffect(() => {
+            if(selectedSpecialty === ''){
+                setFilteredDoctors(allDoctors);
+            }
+            else{
+                const filtered = allDoctors.filter(doc =>
+                    doc.specialty === selectedSpecialty
+                );
+
+                setFilteredDoctors(filtered);
+            }
+        }, [selectedSpecialty, allDoctors]);
+
+    
+    function List(){
+       return(
+       <ul>
+        {filteredDoctors.map(doc => (
+            <li key = {doc.id}>
+                 <p><b>{doc.name}</b></p>
+            </li>
+        ))}
+       </ul>
+       );
+    }
+
+
+    return(
+        <div>
+            <h1>Our Doctors</h1>
+            
+            <label htmlFor ="specialty-select">Choose doctor by specialization</label>
+            <select
+                id = "specialty-select"
+                value = {selectedSpecialty}
+                onChange = {(e) => setSelectedSpecialty(e.target.value)}
+            >
+
+                <option value = "">All</option>
+                <option value = "Pediatrics">Pediatrics</option>
+                <option value = "Cardiology">Cardiology</option>
+                <option value = "Neurology">Neurology</option>
+                <option value = "Trauma Surgeon">Trauma Surgeon</option>
+                
+            </select>
+
+            <List />
+
+        </div>
+    )
+
 }
 
 export default Homepage;
