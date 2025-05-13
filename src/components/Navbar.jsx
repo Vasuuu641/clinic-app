c0651e7ffc4c9120aba5adddd3634a7d0ad29c91
Vasundhara Ravikumar <vasundhararavi641@gmail.com>
2025-05-13 11:54:44 +0200

    Added some styling to the nav menu
    Ensured the nav menu got positioned to the top left for good UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
+import '../css/Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
+    <nav className = "navbar">
+      <div className = "nav-links">
+
       <Link to="/">Home</Link> | {""}
       <Link to="/doctors">Doctors</Link> | {""}
       <Link to="/book">Book</Link> | {""}
       <Link to="/dashboard">Dashboard</Link> | {""}
       <Link to="/admin">Admin</Link> | {""}
       <Link to="/log">Log</Link> 
+
+      </div>
     </nav>
   );
 }
